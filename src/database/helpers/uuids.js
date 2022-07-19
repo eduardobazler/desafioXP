5c46b531120f7d46366967a3fc874e5a9253b106
eduardobazler <edubazler@gmail.com>
2022-07-19 18:19:45 -0300

    arquivo para a geracao de Ids para inserrir no banco de dados, inicialmente como exemplo

---
+++ src/database/helpers/uuids.js
@@ -0,0 +1,12 @@
+const id1 = 'a28100d7-c383-4c63-951c-fffa5d983ae2'
+const id2 = 'a3c044e5-46ba-4d85-8af8-00f67e945efc'
+const id3 = '0879083a-c82d-43a3-9b4d-3a576d008f50'
+
+
+const idConta1 = '0874183a-c82d-43a3-9b4d-3a576d008f50'
+const idConta2 = '0879083b-c52d-43a3-9b4d-3a576d008f50'
+const idConta3 = '0879083a-c82d-43a3-9b4d-3a566d008f58'
+
+module.exports = {
+  id1, id2, id3, idConta1, idConta2, idConta3
+}
