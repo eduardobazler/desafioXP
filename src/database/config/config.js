771a5799a4452452f2fd791099dc289b624a0a0f
eduardobazler <edubazler@gmail.com>
2022-07-19 18:21:58 -0300

    arquivo de configurcao das migrations para o sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+};
