baa9a3d10d21c62b94b0037770364ebbebe16628
eduardobazler <edubazler@gmail.com>
2022-07-19 18:17:29 -0300

    criacao da tabela Corretora

---
+++ src/database/migrations/20220719153904-create-Corretora.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const CorretoraTable = queryInterface.createTable('Corretora', {
+      acaoId :{
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Acoes',
+          key: 'id'
+        },
+        field: 'acao_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    });
+
+    return CorretoraTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Corretora')
+};
