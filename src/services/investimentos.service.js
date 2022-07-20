da81de667655b16923d1c2d2ea76d6c2f58896d3
eduardobazler <edubazler@gmail.com>
2022-07-20 18:32:57 -0300

    adicionando uma noca funcao na camada de servico para a venda de uma acao

--- src/services/investimentos.service.js
+++ src/services/investimentos.service.js
@@ -6,27 +6,32 @@ const {
   upadteBuyAssetsBroker,
   updateBankBalanceBuy,
   upadateHistoryOrders,
-} = require('./helpres.service/quantityUpdaters');
+  upadteSaleAssetsBroker,
+  updateSaleAssetsAccount,
+} = require('./helpers.service/quantityUpdaters');
 
 const {
+  checkBrokerHasAsset,
   checkQuantityAssetsAvailable,
-  checkBankBalanceUserAvailable
-} = require('./helpres.service/quantityValidators');
+  checkBankBalanceUserAvailable,
+  checkUserHasAsset,
+} = require('./helpers.service/quantityValidators');
 
 const config = require('../database/config/config');
 
 const sequelize = new Sequelize(config.development);
 
 const serviceBuyAssets = async ({ codCliente, codAtivo, qtdeAtivo }) => {
-  const transaction = await sequelize.transaction();
-  const { qtdeAvailable, valorAtivo } = await checkQuantityAssetsAvailable(codAtivo, qtdeAtivo);
+  const { qtdeAvailableBroker, valorAtivo } = await checkBrokerHasAsset(codAtivo);
+  checkQuantityAssetsAvailable(qtdeAtivo, qtdeAvailableBroker);
   const { amountRequired, bankBalance } = await checkBankBalanceUserAvailable(qtdeAtivo, valorAtivo, codCliente);
   
+  const transaction = await sequelize.transaction();
   try {
     await updateBuyAssetsAccount(codCliente, codAtivo, qtdeAtivo, transaction);
-    await upadteBuyAssetsBroker(codAtivo, qtdeAtivo, qtdeAvailable, transaction);
-    await updateBankBalanceBuy({ amountRequired, bankBalance }, codCliente, transaction);
-    await upadateHistoryOrders('venda', codCliente, codAtivo, qtdeAtivo, transaction);
+    await upadteBuyAssetsBroker(codAtivo, qtdeAtivo, qtdeAvailableBroker, transaction);
+    await updateBankBalanceBuy(amountRequired, bankBalance, codCliente, transaction);
+    await upadateHistoryOrders('compra', codCliente, codAtivo, qtdeAtivo, transaction);
 
     await transaction.commit();
     return { codCliente, codAtivo, qtdeAtivo, status: 'executada' }
@@ -36,6 +41,26 @@ const serviceBuyAssets = async ({ codCliente, codAtivo, qtdeAtivo }) => {
   }
 }
 
+const serviceSaleAssets = async ({ codCliente, codAtivo, qtdeAtivo }) => {
+  const qtdeAvailableUser = await checkUserHasAsset(codCliente, codAtivo);
+  checkQuantityAssetsAvailable(qtdeAtivo, qtdeAvailableUser);
+
+  const transaction = await sequelize.transaction();
+  try {
+    await upadteSaleAssetsBroker(codAtivo, qtdeAtivo, transaction);
+    await updateSaleAssetsAccount(codAtivo, qtdeAtivo, qtdeAvailableUser, codCliente, transaction);
+    await upadateHistoryOrders('venda', codCliente, codAtivo, qtdeAtivo, transaction);
+    
+    await transaction.commit();
+    return { codCliente, codAtivo, qtdeAtivo, status: 'executada' };
+  } catch (error) {
+    await transaction.rollback();
+    throwErroWithStatus(error);
+  }
+
+}
+
 module.exports = {
   serviceBuyAssets,
+  serviceSaleAssets,
 }
