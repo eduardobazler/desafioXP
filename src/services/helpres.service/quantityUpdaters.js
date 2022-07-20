cee279fe64581ea4685e2fb261a2d52398ccb79e
eduardobazler <edubazler@gmail.com>
2022-07-20 15:38:09 -0300

    arquivo para funcoes que atualizam os valores das tabelas em uma compra da acao

---
+++ src/services/helpres.service/quantityUpdaters.js
@@ -0,0 +1,54 @@
+const { ContaAcoes, Corretora, Historico, Conta } = require('../../models')
+
+const updateBuyAssetsAccount = async (codCliente, codAtivo, qtdeAtivo, transaction) => {
+  const dataValues = await ContaAcoes.findOne({
+    where: { contaId: codCliente, acaoId: codAtivo }
+  });
+  // caso não exista a relação, cria a relação
+  if(!dataValues) {
+    const createdRelationAsset = ContaAcoes.create({
+      contaId: codCliente, acaoId: codAtivo, quantity: qtdeAtivo
+    }, { transaction });
+    return createdRelationAsset;
+  }
+
+  // caso exista a relação, atualiza a quantidade
+  const newQuantity = qtdeAtivo + dataValues.quantity;
+  const updatedRalationAsset = await ContaAcoes.update({ quantity: newQuantity }, {
+    where: { contaId: codCliente, acaoId: codAtivo },
+    transaction
+  });
+  return updatedRalationAsset;
+}
+
+const upadteBuyAssetsBroker = async (codAtivo, qtdeAtivo, qtdeAvailable, transaction) => {
+  const newQtdeAvailable = qtdeAvailable - qtdeAtivo;
+  await Corretora.update({ quantity: newQtdeAvailable }, {
+    where: { acaoId: codAtivo },
+    transaction
+  });
+}
+
+const updateBankBalanceBuy = async ({ amountRequired, bankBalance }, codCliente, transaction) => {
+  const newBankBalance = bankBalance - amountRequired;
+  await Conta.update({ bankBalance: newBankBalance }, {
+    where: { id: codCliente },
+    transaction
+  })
+}
+
+const upadateHistoryOrders = async (type, codCliente, codAtivo, qtdeAtivo, transaction) => {
+  await Historico.create({ 
+    contaId: codCliente,
+    acaoId: codAtivo,
+    quantity: qtdeAtivo,
+    typeOrder: type,
+   }, { transaction });
+};
+
+module.exports = {
+  updateBuyAssetsAccount,
+  upadteBuyAssetsBroker,
+  updateBankBalanceBuy,
+  upadateHistoryOrders
+}
