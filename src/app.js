8407f0770aae4b4c468b8cc4a8ba40cd2f166fbf
eduardobazler <edubazler@gmail.com>
2022-07-19 21:43:46 -0300

    configurando a api para receber tratamento de erro e tratamento de página não encontrada

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const handleError = require('./middlewares/handleError.middleware');
+const handPageNotFound = require('./middlewares/handlePageNotFound');
+const router = require('./routers');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
+
+app.use(handPageNotFound);
+
+app.use(handleError);
 
 module.exports = app;
