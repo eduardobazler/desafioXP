66528153a9e80cc51a5733e38a862391cbdddc12
eduardobazler <edubazler@gmail.com>
2022-07-20 15:42:07 -0300

    adicionando a rota de compra de uma acao ao arquico principal de rotas

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const routerInvestimentos = require('./investimentos.router');
 
 const router = express.Router();
+
+router.use('/investimentos', routerInvestimentos);
 
 module.exports = router;
