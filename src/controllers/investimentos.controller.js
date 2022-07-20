3957609be6a38eacafc8dfe12a3e8c06f2bbc289
eduardobazler <edubazler@gmail.com>
2022-07-20 18:34:33 -0300

    adicionando uma nova funcao na camada de controller para a venda de uma acao

--- src/controllers/investimentos.controller.js
+++ src/controllers/investimentos.controller.js
@@ -7,6 +7,14 @@ const buyAssets = async (req, res) => {
   res.status(StatusCodes.OK).send(dataResponse);
 }
 
+const saleAssets = async (req, res) => {
+  const { codCliente, codAtivo, qtdeAtivo } = req.body;
+  const dataResponse  = await serviceInvestimentos.serviceSaleAssets({ codCliente, codAtivo, qtdeAtivo });
+  res.status(StatusCodes.OK).send(dataResponse);
+
+}
+
 module.exports = {
   buyAssets,
+  saleAssets,
 }
