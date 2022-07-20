c9d035873f30bee094cf40b97961d72a72833a8f
eduardobazler <edubazler@gmail.com>
2022-07-20 18:33:58 -0300

    adicionando novas funcoes para a validacao da regra de negocio dentro da camada de servico

--- src/services/helpers.service/quantityValidators.js
+++ src/services/helpers.service/quantityValidators.js
@@ -1,27 +1,40 @@
-const { Corretora, Acao, Conta } = require('../../models')
-const errorWithStatus = require('../../utils/throwErrorWithStatus');
+const { Corretora, Acao, Conta, ContaAcoes } = require('../../models')
+const throwErrorWithStatus = require('../../utils/throwErrorWithStatus');
 const { StatusCodes } = require('http-status-codes');
 
-const checkQuantityAssetsAvailable = async (codAtivo, qtdeAtivo) => {
-  const { quantity, acao: { value } } = await Corretora.findOne({ where: { acaoId: codAtivo },
-  include: { model: Acao, as: 'acao' }});
-  if (qtdeAtivo > quantity) {
-    return errorWithStatus({ 
+
+const checkBrokerHasAsset = async (codAtivo) => {
+  const asset = await Corretora.findOne({ where: { acaoId: codAtivo },
+    include: { model: Acao, as: 'acao' }});
+
+    if (!asset) {
+      return throwErrorWithStatus({ 
+        status: StatusCodes.UNPROCESSABLE_ENTITY,
+        message: 'Informações incorretas.'
+       });
+    }
+
+    const { quantity, acao: { value } } = asset;
+    return {
+      qtdeAvailableBroker: quantity, 
+      valorAtivo: value
+    }
+}
+
+const checkQuantityAssetsAvailable = (qtdeAtivo ,qtdeAvailable) => {
+  if (qtdeAtivo > qtdeAvailable) {
+    return throwErrorWithStatus({ 
       status: StatusCodes.UNPROCESSABLE_ENTITY,
       message: 'Quantidade da ação indisponível'
      });
   }
-  return { 
-    qtdeAvailable: quantity, 
-    valorAtivo: value 
-  };
 };
 
 const checkBankBalanceUserAvailable = async (qtdeAtivo, valorAtivo, contaId) => {
   const amountRequired = (qtdeAtivo * valorAtivo);
   const { bankBalance } = await Conta.findOne({ where: { id: contaId } });
   if (amountRequired > bankBalance) {
-    return errorWithStatus({ 
+    return throwErrorWithStatus({ 
       status: StatusCodes.UNPROCESSABLE_ENTITY,
       message: 'Saldo bancário não disponível para essa transação.'
     });
@@ -29,7 +42,20 @@ const checkBankBalanceUserAvailable = async (qtdeAtivo, valorAtivo, contaId) => 
   return { amountRequired, bankBalance };
 };
 
+const checkUserHasAsset = async (codCliente, codAtivo) => {
+  const asset = await ContaAcoes.findOne({ where: { acaoId: codAtivo, contaId: codCliente } });
+  if (!asset) {
+    return throwErrorWithStatus({
+      status: StatusCodes.UNPROCESSABLE_ENTITY,
+      message: 'Informações incorretas.'
+    });
+  }
+  return asset.quantity;
+};
+
 module.exports = {
+  checkBrokerHasAsset,
   checkQuantityAssetsAvailable,
   checkBankBalanceUserAvailable,
+  checkUserHasAsset
 }
