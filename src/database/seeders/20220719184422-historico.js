e8d6ac49c3bb0c364a19ba38c5674627c7683ab7
eduardobazler <edubazler@gmail.com>
2022-07-20 13:51:43 -0300

    corrigindo arquivo do seed de historico

--- src/database/seeders/20220719184422-historico.js
+++ src/database/seeders/20220719184422-historico.js
@@ -5,36 +5,42 @@ module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('Historico',
       [{
+        id: 1,
         conta_id: idConta1,
         acao_id: 1,
         quantity: 15,
         type_order: 'compra',
       },
       {
+        id: 2,
         conta_id: idConta1,
         acao_id: 2,
         quantity: 25,
         type_order: 'compra',
       },
       {
+        id: 3,
         conta_id: idConta2,
         acao_id: 2,
         quantity: 30,
         type_order: 'compra',
       },
       {
+        id: 4,
         conta_id: idConta2,
         acao_id: 1,
         quantity: 60,
         type_order: 'compra',
       },
       {
+        id: 5,
         conta_id: idConta2,
         acao_id: 3,
         quantity: 12,
         type_order: 'compra',
       },
       {
+        id: 6,
         conta_id: idConta3,
         acao_id: 2,
         quantity: 18,
