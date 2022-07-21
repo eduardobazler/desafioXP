a79b0fb8b9e5268fc569a4ac6eb5618144e8a594
eduardobazler <edubazler@gmail.com>
2022-07-21 10:31:27 -0300

    rotas adicionadas para saque, ver saldo e ver acoes

--- src/routers/conta.router.js
+++ src/routers/conta.router.js
@@ -4,6 +4,12 @@ require('express-async-errors');
 
 const contaRouter = express.Router();
 
-contaRouter.post('/deposito', contaController.accountDeposit)
+contaRouter.post('/deposito', contaController.accountDeposit);
+
+contaRouter.post('/saque', contaController.accountWithdrawal);
+
+contaRouter.get('/ativos/:id', contaController.getAssets)
+
+contaRouter.get('/:id', contaController.getBankBalance);
 
 module.exports = contaRouter;
