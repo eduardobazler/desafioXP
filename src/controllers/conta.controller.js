890224f8357ce938672a4a149081393e93a8e96c
eduardobazler <edubazler@gmail.com>
2022-07-21 10:30:54 -0300

    camada de controller para saque, verificar o saldo e verificar as acoes que pertencem a conta

--- src/controllers/conta.controller.js
+++ src/controllers/conta.controller.js
@@ -7,6 +7,27 @@ const accountDeposit = async (req, res) => {
   res.status(StatusCodes.OK).json(dataResponse);
 };
 
+const accountWithdrawal = async (req, res) => {
+  const { codCliente, valor } = req.body;
+  const dataResponse = await contaService.accountWithdrawal({ codCliente, valor });
+  res.status(StatusCodes.OK).json(dataResponse);
+}
+
+const getBankBalance = async (req, res) => {
+  const { id: codCliente } = req.params;
+  const dataResponse = await contaService.getBankBalance(codCliente);
+  res.status(StatusCodes.OK).json(dataResponse);
+}
+
+const getAssets = async (req, res) => {
+  const { id: codCliente } = req.params;
+  const dataResponse = await contaService.getAssets(codCliente);
+  res.status(StatusCodes.OK).json(dataResponse);
+}
+
 module.exports = {
   accountDeposit,
+  accountWithdrawal,
+  getBankBalance,
+  getAssets,
 }
