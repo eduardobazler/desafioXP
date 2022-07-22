484228d42d88c80c9f3111525956fcb9d8c06c61
eduardobazler <edubazler@gmail.com>
2022-07-22 16:08:31 -0300

    modificacao do seed para gerar senha criptografda

--- src/database/seeders/20220719181213-users.js
+++ src/database/seeders/20220719181213-users.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
+const generateHash = require('../../utils/generateHash');
 const ids = require('../helpers/uuids');
+
+const senha1 = generateHash('user1');
+const senha2 = generateHash('user2');
+const senha3 = generateHash('user3');
 
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
@@ -8,21 +13,21 @@ module.exports = {
         id: ids.id1,
         user_name: 'Lewis Hamilton',
         email: 'lewishamilton@gmail.com',
-        password: '123456',
+        password: senha1,
         role: 'user',
       },
       {
         id: ids.id2,
         user_name: 'Cleiton do TI',
         email: 'cleiton@ti.com',
-        password: '123456',
-        role: 'user',
+        password: senha2,
+        role: 'admin',
       },
       {
         id: ids.id3,
         user_name: 'Vitoria Santos',
         email: 'vivi@santos.com',
-        password: '123456',
+        password: senha3,
         role: 'user',
       },
       ], { timestamps: false });
