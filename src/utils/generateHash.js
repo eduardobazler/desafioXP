eccd40b29b4d09acb019ce7d81d51d6f85b03deb
eduardobazler <edubazler@gmail.com>
2022-07-22 16:05:09 -0300

    adicionando arquivo ara a criptografia de senhas

---
+++ src/utils/generateHash.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt');
+
+const generateHash = (dado) => {
+  const salt = bcrypt.genSaltSync(5);
+  const dadoCrypt = bcrypt.hashSync(dado, salt);
+  return dadoCrypt;
+};
+
+module.exports = generateHash;
