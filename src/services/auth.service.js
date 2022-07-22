ee215cf654b774c47c8c27fa15e44075d9348fff
eduardobazler <edubazler@gmail.com>
2022-07-22 16:06:04 -0300

    camada de servico para a autenticacao do usuario

---
+++ src/services/auth.service.js
@@ -0,0 +1,44 @@
+const { StatusCodes } = require('http-status-codes');
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const { generateToken } = require('../utils/JWTtoken');
+const throwErroWithStatus = require('../utils/throwErrorWithStatus');
+
+const comparePassword = (reqPass, dbPass) => {
+  const isMath = bcrypt.compareSync(reqPass, dbPass);
+  if (!isMath) {
+    return throwErroWithStatus({
+      status: StatusCodes.BAD_REQUEST, message: 'Invalid fields'
+    });
+  }
+  return isMath;
+}
+
+const authUser = async ({ email, password }) => {
+  const user = await User.findOne({
+    where: { email },
+    attributes: [ 'id', 'email', 'role', 'userName', 'password' ]
+  });
+
+  if (!user) {
+    return throwErroWithStatus({ status: StatusCodes.BAD_REQUEST, message: 'Invalid fields' });
+  }
+
+  const { dataValues } = user;
+
+  comparePassword(password, dataValues.password);
+  
+  token = generateToken({
+    id: dataValues.id,
+    role: dataValues.role,
+    email: dataValues.email,
+    userName: dataValues.userName
+  });
+
+  return token;
+};
+
+
+module.exports = {
+  authUser,
+}
