2b083a81416987242a566cb411d38980b32cf89e
eduardobazler <edubazler@gmail.com>
2022-07-23 09:59:42 -0300

    adicionando cors no app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const handleError = require('./middlewares/handleError.middleware');
 const handPageNotFound = require('./middlewares/handlePageNotFound');
 const router = require('./routers');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
